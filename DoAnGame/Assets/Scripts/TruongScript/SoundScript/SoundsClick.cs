fc4f53aa61bb3e71788a098c35e07f87ffa0ad6e
truongntps18688 <81845787+truongntps18688@users.noreply.github.com>
2022-08-12 21:06:40 +0700

    truong37
    add new positionChest

--- DoAnGame/Assets/Scripts/TruongScript/SoundScript/SoundsClick.cs
+++ DoAnGame/Assets/Scripts/TruongScript/SoundScript/SoundsClick.cs
@@ -6,11 +6,9 @@ using UnityEngine.UI;
 public class SoundsClick : MonoBehaviour
 {
     AudioSource m_sound_click;
-    public AudioClip _sound_click;
     void Start()
     {
         m_sound_click = GetComponent<AudioSource>();
-        m_sound_click.clip = _sound_click;
 
         float[] sound = JsonManager.Instance.getSounds();// 0 : SFX // 1 : BGM
         m_sound_click.volume = sound[0];

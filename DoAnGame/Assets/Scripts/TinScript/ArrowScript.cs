9b85a72b1461475827027cc05c1b7f99c8c8c549
duytin095 <duytin095@gmail.com>
2022-09-22 00:01:31 +0700

    crossbow update

--- DoAnGame/Assets/Scripts/TinScript/ArrowScript.cs
+++ DoAnGame/Assets/Scripts/TinScript/ArrowScript.cs
@@ -6,22 +6,29 @@ public class ArrowScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     private bool isStickAble = true;
+    private TrailRenderer trailRenderer;
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        trailRenderer = gameObject.GetComponent<TrailRenderer>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.layer == 8)
         {
+            rb.isKinematic = true;
             rb.velocity = Vector2.zero;
             isStickAble = false;
-            
+            trailRenderer.enabled = false;
+
         }
         if (collision.gameObject.layer == 11 && isStickAble)
         {
             rb.isKinematic = true;
-            this.transform.parent = collision.transform; 
+            
+            this.transform.parent = collision.transform;
+            trailRenderer.enabled = false;
+
         }
 
 

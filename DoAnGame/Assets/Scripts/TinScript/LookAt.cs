9a531db0f6b24afc9e12a7cdbdcb641f8f7b4e01
duytin095 <duytin095@gmail.com>
2022-10-11 15:37:07 +0700

    điểm nhìn
    Camera không còn lấy player làm trung tâm nữa mà lấy một điểm khác, giờ đây camera có thể lắc đi (có tầm nhìn) xa hơn

---
+++ DoAnGame/Assets/Scripts/TinScript/LookAt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Transform player;
+    [SerializeField] float threshold;
+    private void Awake()
+    {
+        cam = FindObjectOfType<Camera>();
+        player = FindObjectOfType<Player>().gameObject.transform;
+    }
+    void Update()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPos = (player.position + mousePos) / 2;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + player.position.x, threshold + player.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+
+        this.transform.position = targetPos;
+    }
+}

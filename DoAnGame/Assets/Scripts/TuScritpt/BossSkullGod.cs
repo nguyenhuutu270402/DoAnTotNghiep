a161e74e79ad8fc4c0dd49020849463d2f54b381
nguyenhuutu0427 <95615938+nguyenhuutu270402@users.noreply.github.com>
2022-10-15 00:18:02 +0700

    update boss 13

--- DoAnGame/Assets/Scripts/TuScritpt/BossSkullGod.cs
+++ DoAnGame/Assets/Scripts/TuScritpt/BossSkullGod.cs
@@ -2,23 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using System.Threading;
 
 
 public class BossSkullGod : MonoBehaviour
 {
-    public float HP, timeRe;
+    public float HP, timeRe, SpeedBullet, SpeedRotation, timeAttack2, numAttack2, timeAttack3, numAttack3;
     public AILerp aILerp;
-    public GameObject explosionClassic, explosionBazoka, bullet1, bullet2;
+    public GameObject explosionClassic, explosionBazoka, bullet1, bullet2, effBuffHP;
     public Animator animator;
-    bool die = false, isAttack = false;
+    bool die = false;
     float timeLoop;
     public GameObject enemySkull;
     public Transform point1, point2, point3, point4;
+    float x, y, angel = 0, radius = 10;
+    float xBullet, yBullet;
+    float nAttack2, nAttack3, attack = 0, speedBoss;
+
 
     // Start is called before the first frame update
     void Start()
     {
         timeLoop = timeRe;
+        nAttack2 = numAttack2;
+        nAttack3 = numAttack3;
+        speedBoss = aILerp.speed;
     }
 
     // Update is called once per frame
@@ -35,10 +43,30 @@ public class BossSkullGod : MonoBehaviour
 
 
         timeLoop -= Time.deltaTime;
-        if(timeLoop <= 0 & isAttack == false & die == false)
+        if (timeLoop <= 0 & attack == 0 & die == false)
         {
-            isAttack = true;
+            attack = 4;
+            //numAttack3 = nAttack3;
+        }
+
+
+
+
+        if (attack == 1)
+        {
+            attack1();
+        }
+        if (attack == 2)
+        {
             attack2();
+        }
+        if (attack == 3)
+        {
+            attack3();
+        }
+        else
+        {
+            attack4();
         }
     }
 
@@ -50,17 +78,89 @@ public class BossSkullGod : MonoBehaviour
         Instantiate(enemySkull, point3.position, Quaternion.identity);
         Instantiate(enemySkull, point4.position, Quaternion.identity);
         timeLoop = timeRe;
-        isAttack = false;
+        attack = 0;
     }
 
     void attack2()
     {
-        
+        angel = angel + Time.deltaTime * SpeedRotation;
+        x = transform.position.x + Mathf.Cos(angel) * radius;
+        y = transform.position.y + Mathf.Sin(angel) * radius;
+        if (angel >= 360)
+        {
+            angel = 0;
+        }
+
+
+        timeLoop -= Time.deltaTime;
+        if (timeLoop <= 0)
+        {
+            numAttack2--;
+
+            xBullet = x - transform.position.x;
+            yBullet = y - transform.position.y;
+            GameObject bullet = Instantiate(bullet1, transform.position, Quaternion.identity);
+            Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+            rbBullet.velocity = new Vector2(xBullet * SpeedBullet, yBullet * SpeedBullet);
+            bullet.transform.right = rbBullet.velocity;
+
+            GameObject bullet2 = Instantiate(bullet1, transform.position, Quaternion.identity);
+            Rigidbody2D rbBullet2 = bullet2.GetComponent<Rigidbody2D>();
+            rbBullet2.velocity = new Vector2(xBullet * -SpeedBullet, yBullet * -SpeedBullet);
+            bullet2.transform.right = rbBullet2.velocity;
+            timeLoop = timeAttack2;
+        }
+        if (numAttack2 <= 0)
+        {
+            attack = 0;
+            timeLoop = timeRe;
+        }
     }
 
     void attack3()
     {
+        timeLoop -= Time.deltaTime;
+        if (timeLoop <= 0)
+        {
+            numAttack3--;
 
+            GameObject b1 = Instantiate(bullet2, point1.position, Quaternion.identity);
+            Rigidbody2D rb1 = b1.GetComponent<Rigidbody2D>();
+            rb1.velocity = new Vector2(0, 2);
+            b1.transform.right = rb1.velocity;
+
+            GameObject b2 = Instantiate(bullet2, point2.position, Quaternion.identity);
+            Rigidbody2D rb2 = b2.GetComponent<Rigidbody2D>();
+            rb2.velocity = new Vector2(0, -2);
+            b2.transform.right = rb2.velocity;
+
+            GameObject b3 = Instantiate(bullet2, point3.position, Quaternion.identity);
+            Rigidbody2D rb3 = b3.GetComponent<Rigidbody2D>();
+            rb3.velocity = new Vector2(2, 0);
+            b3.transform.right = rb3.velocity;
+
+            GameObject b4 = Instantiate(bullet2, point4.position, Quaternion.identity);
+            Rigidbody2D rb4 = b4.GetComponent<Rigidbody2D>();
+            rb4.velocity = new Vector2(-2, 0);
+            b4.transform.right = rb4.velocity;
+
+            timeLoop = timeAttack3;
+        }
+        if (numAttack3 <= 0)
+        {
+            attack = 0;
+            timeLoop = timeRe;
+        }
+    }
+
+    void attack4()
+    {
+        Instantiate(effBuffHP, transform.position, Quaternion.identity);
+        aILerp.speed = 0;
+        Thread.Sleep(2000);
+        HP += 10;
+        aILerp.speed = speedBoss;
+        attack = 0;
     }
 
 

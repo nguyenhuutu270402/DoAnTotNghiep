4b99ff400d48bdafe96984e3e10523a83ae4647e
nguyenhuutu0427 <95615938+nguyenhuutu270402@users.noreply.github.com>
2022-10-20 20:51:39 +0700

    sửa hố đen + đạn boss

--- DoAnGame/Assets/Scripts/TuScritpt/BlackHole.cs
+++ DoAnGame/Assets/Scripts/TuScritpt/BlackHole.cs
@@ -8,9 +8,10 @@ public class BlackHole : MonoBehaviour
     float timeLoop = 5;
     public float timeRe;
     public GameObject enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9, enemy10, enemy11;
+    public Animator animator;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,55 +25,61 @@ public class BlackHole : MonoBehaviour
         timeLoop -= Time.deltaTime;
         if (timeLoop < 0)
         {
-            int rd = Random.Range(1, 10);
-            if(rd == 1)
-            {
-                Instantiate(enemy1, transform.position, Quaternion.identity);
-            }
-            if (rd == 2)
-            {
-                Instantiate(enemy2, transform.position, Quaternion.identity);
-            }
-            if (rd == 3)
-            {
-                Instantiate(enemy3, transform.position, Quaternion.identity);
-            }
-            if (rd == 4)
-            {
-                Instantiate(enemy4, transform.position, Quaternion.identity);
-            }
-            if (rd == 5)
-            {
-                Instantiate(enemy5, transform.position, Quaternion.identity);
-            }
-            if (rd == 6)
-            {
-                Instantiate(enemy6, transform.position, Quaternion.identity);
-            }
-            if (rd == 7)
-            {
-                Instantiate(enemy7, transform.position, Quaternion.identity);
-            }
-            if (rd == 8)
-            {
-                Instantiate(enemy8, transform.position, Quaternion.identity);
-            }
-            if (rd == 9)
-            {
-                Instantiate(enemy9, transform.position, Quaternion.identity);
-            }
-            if (rd == 10)
-            {
-                Instantiate(enemy10, transform.position, Quaternion.identity);
-            }
-            if (rd == 11)
-            {
-                Instantiate(enemy11, transform.position, Quaternion.identity);
-            }
-
-
-            timeLoop = timeRe;
+            animator.SetBool("isWork", true);
         }
-            
+
+    }
+
+    public void createEnemy()
+    {
+        int rd = Random.Range(1, 12);
+        if (rd == 1)
+        {
+            Instantiate(enemy11, transform.position, Quaternion.identity);
+        }
+        else if(rd == 2)
+        {
+            Instantiate(enemy2, transform.position, Quaternion.identity);
+        }
+        else if(rd == 3)
+        {
+            Instantiate(enemy3, transform.position, Quaternion.identity);
+        }
+        else if(rd == 4)
+        {
+            Instantiate(enemy4, transform.position, Quaternion.identity);
+        }
+        else if(rd == 5)
+        {
+            Instantiate(enemy5, transform.position, Quaternion.identity);
+        }
+        else if(rd == 6)
+        {
+            Instantiate(enemy6, transform.position, Quaternion.identity);
+        }
+        else if(rd == 7)
+        {
+            Instantiate(enemy7, transform.position, Quaternion.identity);
+        }
+        else if(rd == 8)
+        {
+            Instantiate(enemy8, transform.position, Quaternion.identity);
+        }
+        else if(rd == 9)
+        {
+            Instantiate(enemy9, transform.position, Quaternion.identity);
+        }
+        else if(rd == 10)
+        {
+            Instantiate(enemy10, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemy1, transform.position, Quaternion.identity);
+        }
+
+
+        timeLoop = timeRe;
+        animator.SetBool("isWork", false);
     }
 }

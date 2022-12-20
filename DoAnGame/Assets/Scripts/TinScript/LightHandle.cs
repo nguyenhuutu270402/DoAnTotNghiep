2a54767a4709a78695c74aff1e36281af822ddc2
duytin095 <duytin095@gmail.com>
2022-12-21 00:14:39 +0700

    light for map 3, cam size, intro, bg music for all maps

---
+++ DoAnGame/Assets/Scripts/TinScript/LightHandle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LightHandle : MonoBehaviour
+{
+
+    [SerializeField] private Light globalLight;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DayRountine()
+    {
+        
+    }
+
+}
